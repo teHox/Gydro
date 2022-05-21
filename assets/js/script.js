bc30e51f9397f5e6c886e227320ec0242346e357
teHox <61248895+teHox@users.noreply.github.com>
2022-05-21 15:44:23 +0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,19 +4,24 @@ let burgerBtn = document.querySelector(".burger-btn");
 let headerMenu = document.querySelector(".header__menu");
 
 burgerBtn.addEventListener("click", () => {
-    burgerBtn.classList.toggle("burger-btn_active");
-    headerMenu.classList.toggle("header__menu_active");
+  burgerBtn.classList.toggle("burger-btn_active");
+  headerMenu.classList.toggle("header__menu_active");
 });
 
-let faqPlus = document.querySelector(".faq__plus");
-let faqLine = document.querySelector(".faq__line");
-let faqText = document.querySelector(".faq__text");
+const spoilers = document.querySelectorAll(".faq__question");
 
-faqPlus.addEventListener("click", () => {
+spoilers.forEach((e) => {
+  const faqPlus = e.querySelector(".faq__plus");
+  const faqLine = e.querySelector(".faq__line");
+  const faqText = e.querySelector(".faq__text");
+
+  faqPlus.addEventListener("click", () => {
     faqPlus.classList.toggle("faq__plus_active");
     faqLine.classList.toggle("faq__line_active");
     faqText.classList.toggle("faq__text_active");
+  });
 });
+
 // for (let i = 0; i < picturesList.length; i++) {
 //     picturesList[i].addEventListener('click', function () {
 //       showBigPicture(pictures[i]);
